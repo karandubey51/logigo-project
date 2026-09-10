650aadc3e9562746b0b446ea53ba57691d340e95
karandubey51 <karandubey5123@gmail.com>
2026-09-11 04:50:33 +0530

    Update server.js

--- logigo/backend/server.js
+++ logigo/backend/server.js
@@ -10,7 +10,7 @@ const adminRoutes = require('./routes/adminRoutes');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '2mb' }));
 
 // Serve the frontend static files
 app.use(express.static('../frontend'));
